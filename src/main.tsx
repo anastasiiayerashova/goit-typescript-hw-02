cfd6d3ade0943a3a7420e8ce08db70953c7e55a8
Anastasiia Yerashova <yerashova.a@gmail.com>
2024-12-25 15:11:29 +0100

    upd

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,8 @@ import 'modern-normalize';
 import App from "./components/App.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
